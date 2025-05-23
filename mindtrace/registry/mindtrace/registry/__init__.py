72b3621b1292c1abeb2d2f1353fe7f19764a7b72
Jeremy Wurbs <jeremy@tegi.ai>
2025-05-23 13:18:00 +0100

    Add Archiver template class

--- mindtrace/registry/mindtrace/registry/__init__.py
+++ mindtrace/registry/mindtrace/registry/__init__.py
@@ -3,5 +3,6 @@ from mindtrace.registry.backends.gcp_registry_backend import GCPRegistryBackend
 from mindtrace.registry.backends.local_registry_backend import LocalRegistryBackend
 from mindtrace.registry.backends.minio_registry_backend import MinioRegistryBackend
 from mindtrace.registry.core.registry import Registry
+from mindtrace.registry.core.archiver import Archiver
 
-__all__ = ["LocalRegistryBackend", "GCPRegistryBackend", "MinioRegistryBackend", "Registry", "RegistryBackend"]
+__all__ = ["Archiver", "LocalRegistryBackend", "GCPRegistryBackend", "MinioRegistryBackend", "Registry", "RegistryBackend"]
