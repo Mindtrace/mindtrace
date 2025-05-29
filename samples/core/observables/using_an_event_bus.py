93eaefcc6184d3e5ec3037d4b6bdc8c5672c0b41
Jeremy Wurbs <jdwurbs@gmail.com>
2025-05-29 14:54:17 +0100

    Add samples for observable module

---
+++ samples/core/observables/using_an_event_bus.py
@@ -0,0 +1,28 @@
+from mindtrace.core import EventBus
+
+
+class Source:
+    def __init__(self, event_bus: EventBus):
+        self.event_bus = event_bus
+
+    def emit(self):
+        self.event_bus.emit(event_name="event", data={"key": "value"})
+
+
+class Listener:
+    def __init__(self, event_bus: EventBus):
+        event_bus.subscribe("event", self.on_event)
+
+    def on_event(self, **kwargs):
+        print(f"Received event: {kwargs}")
+
+
+event_bus = EventBus()
+source = Source(event_bus)
+listener = Listener(event_bus)
+
+source.emit()
+
+
+# Output:
+# Received event: {'data': {'key': 'value'}}
