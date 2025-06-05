15528b55104eb54947a04d9a7aa1a2bd9fdfa06f
Jeremy Wurbs <jeremy@tegi.ai>
2025-06-05 22:43:27 +0100

    Add a RegistryBackend class

--- mindtrace/core/mindtrace/core/__init__.py
+++ mindtrace/core/mindtrace/core/__init__.py
@@ -1,18 +1,25 @@
 from mindtrace.core.utils.checks import ifnone, first_not_none
+from mindtrace.core.config import Config
 from mindtrace.core.base.mindtrace_base import Mindtrace, MindtraceABC, MindtraceMeta
 from mindtrace.core.observables.event_bus import EventBus
 from mindtrace.core.observables.observable_context import ObservableContext
 from mindtrace.core.observables.context_listener import ContextListener
 from mindtrace.core.registry.archiver import Archiver
+from mindtrace.core.registry.backends.registry_backend import RegistryBackend
+from mindtrace.core.registry.backends.local_backend import LocalRegistryBackend
+
 
 __all__ = [
+    "Archiver",
     "ContextListener", 
+    "Config",
     "EventBus", 
     "first_not_none", 
     "ifnone", 
+    "LocalRegistryBackend",
     "Mindtrace", 
     "MindtraceABC", 
     "MindtraceMeta",
     "ObservableContext",
-    "Archiver"
+    "RegistryBackend",
 ]
--- mindtrace/core/mindtrace/core/config/config.py
+++ mindtrace/core/mindtrace/core/config/config.py
@@ -1,3 +1,6 @@
 def Config():
     """Template Config "class"."""
-    return {}
+    return {
+        "MINDTRACE_TEMP_DIR": "~/.cache/mindtrace/temp",
+        "MINDTRACE_DEFAULT_REGISTRY_DIR": "~/.cache/mindtrace/registry",
+    }
