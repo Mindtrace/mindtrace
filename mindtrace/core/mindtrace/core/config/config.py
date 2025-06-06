295cf613affe21f35e65ec68680ae4be3bf17009
Jeremy Wurbs <jeremy@tegi.ai>
2025-06-06 23:08:04 +0100

    Update Config template class to be an actual class

--- mindtrace/core/mindtrace/core/config/config.py
+++ mindtrace/core/mindtrace/core/config/config.py
@@ -1,6 +1,11 @@
-def Config():
-    """Template Config "class"."""
-    return {
-        "MINDTRACE_TEMP_DIR": "~/.cache/mindtrace/temp",
-        "MINDTRACE_DEFAULT_REGISTRY_DIR": "~/.cache/mindtrace/registry",
-    }
+class Config(dict):
+    """Template Config class."""
+    
+    def __init__(self, **kwargs):
+        default_config = {
+            "MINDTRACE_TEMP_DIR": "~/.cache/mindtrace/temp",
+            "MINDTRACE_DEFAULT_REGISTRY_DIR": "~/.cache/mindtrace/registry",
+        }
+        # Update defaults with any provided kwargs
+        default_config.update(kwargs)
+        super().__init__(default_config)
