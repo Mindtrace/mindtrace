a782077a705fd2f7eed999d552f10f940e991a8d
pankhuri-mt <pankhuri@mindtrace.ai>
2025-06-09 13:17:11 +0100

    add usage samples

---
+++ samples/core/logging/using_core_logging_format_from_base_class.py
@@ -0,0 +1,17 @@
+from mindtrace.core import Mindtrace
+
+class MyClass(Mindtrace):
+    def __init__(self):
+        super().__init__()
+
+    def instance_method(self):
+        self.logger.info(f"Find this log in `~/.cache/mindtrace/mindtrace.log`. Using instance logger: {self.logger.name}")  # Using logger: mindtrace.my_module.MyClass
+
+    @classmethod
+    def class_method(cls):
+        cls.logger.info(f"Find this log in `~/.cache/mindtrace/mindtrace.log`. Using cls logger: {cls.logger.name}")  # Using logger: mindtrace.my_module.MyClass
+
+if __name__=="__main__":
+    cls_insance = MyClass()
+    cls_insance.instance_method()
+    cls_insance.class_method()
