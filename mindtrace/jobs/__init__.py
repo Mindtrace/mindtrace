3291cdb2bbe0471b7b4e6d787e681eaf863c424d
Joshua-Lister98 <joshua@mindtrace.ai>
2025-06-10 09:56:30 +0100

    Removing example code and update to job and orchestrator

--- mindtrace/jobs/__init__.py
+++ mindtrace/jobs/__init__.py
@@ -1,11 +1,11 @@
-from mindtrace.jobs.mindtrace.types import Job, JobSchema, ExecutionStatus, BackendType
+from mindtrace.jobs.mindtrace.types import Job, JobSchema, JobInput, JobOutput, ExecutionStatus, BackendType
 from mindtrace.jobs.mindtrace.queue_management.orchestrator import Orchestrator
 from mindtrace.jobs.mindtrace.queue_management.local.client import LocalClient
 from mindtrace.jobs.mindtrace.queue_management.redis.client import RedisClient
 from mindtrace.jobs.mindtrace.queue_management.rabbitmq.client import RabbitMQClient
 
 __all__ = [
-    'Job', 'JobSchema', 'ExecutionStatus', 'BackendType',
+    'Job', 'JobSchema', 'JobInput', 'JobOutput', 'ExecutionStatus', 'BackendType',
     'Orchestrator', 
     'LocalClient', 'RedisClient', 'RabbitMQClient'
 ] 
--- mindtrace/jobs/mindtrace/__init__.py
+++
@@ -1,9 +0,0 @@
-from mindtrace.jobs.mindtrace.types import Job, JobSchema, ExecutionStatus, BackendType
-from mindtrace.jobs.mindtrace.queue_management import Orchestrator, LocalClient, RedisClient, RabbitMQClient
-from mindtrace.jobs.mindtrace import utils
-
-__all__ = [
-    'Job', 'JobSchema', 'ExecutionStatus', 'BackendType',
-    'Orchestrator', 'LocalClient', 'RedisClient', 'RabbitMQClient',
-    'utils'
-] 
