d9434c2d59c1d46986c847fe07263b2fd48ce6d1
Jeremy Wurbs <jeremy@tegi.ai>
2025-06-12 14:44:21 +0100

    Reorder tests to run fast to slow: (1) unit, (2) unit marked slow, (3) integration, (4) integration marked slow

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,16 @@ empty_mark = Mark('', [], {})
 
 
 def by_slow_marker(item):
-    return 0 if item.get_closest_marker("slow") is None else 1
+    # Check if test is marked as slow
+    is_slow = 0 if item.get_closest_marker("slow") is None else 1
+    
+    # Check if test is integration test
+    is_integration = 1 if "integration" in str(item.fspath) else 0
+    
+    # Return tuple for sorting: (is_integration, is_slow)
+    # This will sort unit tests first, then slow unit tests,
+    # then integration tests, then slow integration tests
+    return (is_integration, is_slow)
 
 
 def pytest_addoption(parser):
