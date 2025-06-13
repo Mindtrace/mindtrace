2b8c6886e72b633de65d29d1bf700c24c8ff44ad
Jeremy Wurbs <jeremy@tegi.ai>
2025-06-13 13:10:59 +0100

    Add Registry unit tests

--- mindtrace/registry/mindtrace/registry/__init__.py
+++ mindtrace/registry/mindtrace/registry/__init__.py
@@ -4,13 +4,14 @@ from mindtrace.registry.backends.registry_backend import RegistryBackend
 from mindtrace.registry.backends.gcp_registry_backend import GCPRegistryBackend
 from mindtrace.registry.backends.local_registry_backend import LocalRegistryBackend
 from mindtrace.registry.backends.minio_registry_backend import MinioRegistryBackend
-from mindtrace.registry.core.registry import Registry
+from mindtrace.registry.core.registry import LockTimeoutError, Registry
 
 
 __all__ = [
     "Archiver", 
     "ConfigArchiver", 
     "LocalRegistryBackend", 
+    "LockTimeoutError",
     "GCPRegistryBackend", 
     "MinioRegistryBackend", 
     "Registry", 
