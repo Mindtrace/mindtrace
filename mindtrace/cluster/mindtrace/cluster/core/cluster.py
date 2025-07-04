6242dd77cf8c40c4d9fb830128b769c98efe6acb
christopherfish <christopher.fish@mindtrace.ai>
2025-07-04 11:05:27 +0100

    Add docstrings

--- mindtrace/cluster/mindtrace/cluster/core/cluster.py
+++ mindtrace/cluster/mindtrace/cluster/core/cluster.py
@@ -23,10 +23,25 @@ class ClusterManager(Gateway):
         )
 
     def register_job_to_endpoint(self, payload: cluster_types.RegisterJobToEndpointInput):
+        """
+        Register a job to an endpoint.
+
+        Args:
+            payload (RegisterJobToEndpointInput): The payload containing the job type and endpoint.
+        """
         self._job_registry[payload.job_type] = payload.endpoint
         self._registry.save("jobregistry", self._job_registry)
 
     def _submit_job_to_endpoint(self, job: Job):
+        """
+        Submit a job to the appropriate endpoint.
+
+        Args:
+            job (Job): The job to submit.
+
+        Returns:
+            JobOutput: The output of the job.
+        """
         endpoint_url = f"{self._url}{self._job_registry[job.schema_name]}"
         print(endpoint_url)
         response = requests.post(endpoint_url, json=job.payload, timeout=60)
@@ -43,6 +58,15 @@ class ClusterManager(Gateway):
         return cluster_types.JobOutput(status="success", output=result)
 
     def submit_job(self, job: Job):
+        """
+        Submit a job to the cluster. Will route to the appropriate endpoint based on the job type, or to the Orchestrator once that is implemented.
+
+        Args:
+            job (Job): The job to submit.
+
+        Returns:
+            JobOutput: The output of the job.
+        """
         if job.schema_name in self._job_registry:
             return self._submit_job_to_endpoint(job)
         else:
