40650fd020f241fb659797e46c1a9e0bf9e77b8f
Jeremy Wurbs <jeremy@tegi.ai>
2025-07-23 18:15:43 +0100

    Add YoloArchiver to the mindtrace-registry package

---
+++ mindtrace/registry/mindtrace/registry/archivers/yolo_archiver.py
@@ -0,0 +1,24 @@
+import os
+from typing import Any, ClassVar, Tuple, Type, TYPE_CHECKING
+
+from ultralytics import YOLO
+from zenml.enums import ArtifactType
+
+from mindtrace.registry import Archiver, Registry
+
+
+class YoloArchiver(Archiver):
+    ASSOCIATED_TYPES: ClassVar[Tuple[Type[Any], ...]] = (YOLO,)
+    ASSOCIATED_ARTIFACT_TYPE: ClassVar[ArtifactType] = ArtifactType.DATA
+
+    def __init__(self, uri: str, **kwargs):
+        super().__init__(uri=uri, **kwargs)
+
+    def save(self, model: YOLO):
+        model.save(os.path.join(self.uri, "model.pt"))
+
+    def load(self, data_type: Type[Any]) -> YOLO:
+        return YOLO(os.path.join(self.uri, "model.pt"))
+
+
+Registry.register_default_materializer(YOLO, YoloArchiver)
