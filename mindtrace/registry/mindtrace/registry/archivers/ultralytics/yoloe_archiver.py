4dbe6bedf754e002a818f5163bee908dce56fc33
Jeremy Wurbs <jeremy@tegi.ai>
2025-07-24 09:29:15 +0100

    Add Archivers for Ultralytics SAM and YOLOE models

--- mindtrace/registry/mindtrace/registry/archivers/ultralytics/yoloe_archiver.py
+++ mindtrace/registry/mindtrace/registry/archivers/ultralytics/yoloe_archiver.py
@@ -1,24 +1,24 @@
 import os
 from typing import Any, ClassVar, Tuple, Type, TYPE_CHECKING
 
-from ultralytics import YOLO
+from ultralytics import YOLOE
 from zenml.enums import ArtifactType
 
 from mindtrace.registry import Archiver, Registry
 
 
-class YoloArchiver(Archiver):
-    ASSOCIATED_TYPES: ClassVar[Tuple[Type[Any], ...]] = (YOLO,)
+class YoloEArchiver(Archiver):
+    ASSOCIATED_TYPES: ClassVar[Tuple[Type[Any], ...]] = (YOLOE, )
     ASSOCIATED_ARTIFACT_TYPE: ClassVar[ArtifactType] = ArtifactType.DATA
 
     def __init__(self, uri: str, **kwargs):
         super().__init__(uri=uri, **kwargs)
 
-    def save(self, model: YOLO):
+    def save(self, model: YOLOE):
         model.save(os.path.join(self.uri, "model.pt"))
 
-    def load(self, data_type: Type[Any]) -> YOLO:
-        return YOLO(os.path.join(self.uri, "model.pt"))
+    def load(self, data_type: Type[Any]) -> YOLOE:
+        return YOLOE(os.path.join(self.uri, "model.pt"))
 
 
-Registry.register_default_materializer(YOLO, YoloArchiver)
+Registry.register_default_materializer(YOLOE, YoloEArchiver)
