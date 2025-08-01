2ded16719b1615903f29d9b7d3e93b572f89bc81
canelbirlik <can.elbirlik@mindtrace.ai>
2025-08-01 12:14:59 +0100

    change config management/import .env files

--- mindtrace/ui/mindtrace/ui/poseidon/poseidon/backend/core/config.py
+++ mindtrace/ui/mindtrace/ui/poseidon/poseidon/backend/core/config.py
@@ -1,15 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb://localhost:27017"
-    DB_NAME: str = "mydb"
-    SECRET_KEY: str = ""
-    GCP_BUCKET_NAME: str = ""
-    GCP_CREDENTIALS_PATH: str = ""
+    MONGO_URI: str
+    DB_NAME: str
+    SECRET_KEY: str
+    GCP_BUCKET_NAME: str
+    GCP_CREDENTIALS_PATH: str
+    MODEL_SERVER_URL: str
 
-    MODEL_SERVER_URL: str = ""
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    class Config:
-        env_file = ".env"
 
-settings = Settings() 
+ENV = os.getenv("ENV", "dev")  # dev/prod
+settings = Settings(_env_file=f".env.{ENV}", _env_file_encoding="utf-8")
