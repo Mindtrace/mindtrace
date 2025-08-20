d49fc2e86a3a711f123c83f0bcbc7caccd3fb60c
Yasserelhaddar <yasserelhaddar@gmail.com>
2025-08-20 16:05:32 +0100

    Integrate license management into navigation
    - Add license management sidebar navigation item
    - Register license management page route
    - Update management pages module exports
    - Enable role-based access to license management features

--- mindtrace/apps/mindtrace/apps/poseidon/poseidon/pages/management/__init__.py
+++ mindtrace/apps/mindtrace/apps/poseidon/poseidon/pages/management/__init__.py
@@ -3,9 +3,11 @@
 from .user_management import user_management_page
 from .organization_management import organization_management_page
 from .project_management import project_management_page
+from .license_management import license_management_page
 
 __all__ = [
     "user_management_page",
     "organization_management_page",
     "project_management_page",
+    "license_management_page",
 ] 
