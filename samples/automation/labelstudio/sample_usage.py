c7b42b2ea7f94afdfb2a4ee3aae33c2a70084288
Vikrant Sharma <7.sharma.vikrant@gmail.com>
2025-09-09 12:55:42 +0100

    ruff format

--- samples/automation/labelstudio/sample_usage.py
+++ samples/automation/labelstudio/sample_usage.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from mindtrace.core.config import Config
-from mindtrace.automation.label_studio.label_studio_api import LabelStudio
 
-url="http://localhost:8080"
+from mindtrace.automation.label_studio.label_studio_api import LabelStudio
+from mindtrace.core.config import Config
+
+url = "http://localhost:8080"
 config = Config()
-api_key=config["MINDTRACE_API_KEYS"]["LabelStudio"]
+api_key = config["MINDTRACE_API_KEYS"]["LabelStudio"]
 google_application_credentials = config["MINDTRACE_GCP_CREDENTIALS_PATH"]
 
 label_config = """
@@ -34,29 +35,27 @@ print(f"Created {created} tasks in project '{project.title}' (ID: {project.id})"
 
 # 3.1 Create GCP storage
 try:
-  storage = ls.create_gcp_storage(project_name="test_project", 
-                                  bucket="mt-label-studio-bucket", 
-                                  prefix="samples", storage_type="import", 
-                                  use_blob_urls=True,presign=False,
-                                  google_application_credentials=google_application_credentials,
-                                  regex_filter=".*jpg")
-  print(f"Created GCP storage id: {storage['id']} in project '{project.title}' (ID: {project.id})")
+    storage = ls.create_gcp_storage(
+        project_name="test_project",
+        bucket="mt-label-studio-bucket",
+        prefix="samples",
+        storage_type="import",
+        use_blob_urls=True,
+        presign=False,
+        google_application_credentials=google_application_credentials,
+        regex_filter=".*jpg",
+    )
+    print(f"Created GCP storage id: {storage['id']} in project '{project.title}' (ID: {project.id})")
 except ValueError:
-  print(f"GCP storage already exists in project '{project.title}' (ID: {project.id})")
+    print(f"GCP storage already exists in project '{project.title}' (ID: {project.id})")
+
 # 3.2 Sync GCP storage
-sync_gcp_storage = ls.sync_gcp_storage(project_name="test_project",storage_prefix="samples")
+sync_gcp_storage = ls.sync_gcp_storage(project_name="test_project", storage_prefix="samples")
 print(f"Synced GCP storage in project '{project.title}' (ID: {project.id})")
 
 # 4. Get annotations
 annotations = ls.get_annotations(project_name="test_project")
 for annotation in annotations:
-  for key,value in annotation.items():
-    print(f"{key}:{value}")
-  print("--------------------------------")
-
-
-
-
-
-
-
+    for key, value in annotation.items():
+        print(f"{key}:{value}")
+    print("--------------------------------")
