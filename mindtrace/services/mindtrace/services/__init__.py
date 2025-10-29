8d908d6dc222461991f3a2378481d1ece4a1ff6a
Vikrant Sharma <7.sharma.vikrant@gmail.com>
2025-10-29 12:11:37 +0000

    ruff format

--- mindtrace/services/mindtrace/services/__init__.py
+++ mindtrace/services/mindtrace/services/__init__.py
@@ -1,3 +1,5 @@
+from mindtrace.services.sample.echo_service import EchoService
+
 from mindtrace.services.core.connection_manager import ConnectionManager
 from mindtrace.services.core.service import Service
 from mindtrace.services.core.types import (
@@ -14,7 +16,6 @@ from mindtrace.services.core.utils import generate_connection_manager
 from mindtrace.services.gateway.gateway import Gateway
 from mindtrace.services.gateway.proxy_connection_manager import ProxyConnectionManager
 from mindtrace.services.gateway.types import AppConfig, RegisterAppTaskSchema
-from mindtrace.services.sample.echo_service import EchoService
 
 __all__ = [
     "AppConfig",
