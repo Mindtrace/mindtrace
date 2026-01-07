9fed62a4687a7da8e48310d169266cc0623a04ab
canelbirlik <can.elbirlik@mindtrace.ai>
2026-01-07 11:52:58 +0000

    add to inti

--- mindtrace/storage/mindtrace/storage/__init__.py
+++ mindtrace/storage/mindtrace/storage/__init__.py
@@ -1,10 +1,12 @@
 from mindtrace.storage.base import BatchResult, FileResult, StorageHandler, StringResult
 from mindtrace.storage.gcs import GCSStorageHandler
+from mindtrace.storage.minio import MinioStorageHandler
 
 __all__ = [
     "BatchResult",
     "FileResult",
     "GCSStorageHandler",
+    "MinioStorageHandler",
     "StorageHandler",
     "StringResult",
 ]
