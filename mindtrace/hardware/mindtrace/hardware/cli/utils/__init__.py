f93cec1e4568c08520bc28070c8819f07b3b9f7c
Yasserelhaddar <yasserelhaddar@gmail.com>
2026-01-14 10:39:04 +0000

    refactor(hardware): migrate CLI network utils to core module
    - Update cli/utils/network.py to re-export from mindtrace.core
    - Update CLI commands (camera, plc, stereo) to use exception-based API:
      - Use is_port_available() instead of check_port_available() for bool check
      - Catch ServiceTimeoutError instead of checking return value
    - Export all network exceptions and functions from cli/utils/__init__.py

--- mindtrace/hardware/mindtrace/hardware/cli/utils/__init__.py
+++ mindtrace/hardware/mindtrace/hardware/cli/utils/__init__.py
@@ -1,6 +1,35 @@
 """CLI utility functions."""
 
 from mindtrace.hardware.cli.utils.display import format_status, print_table
-from mindtrace.hardware.cli.utils.network import check_port_available, get_free_port
+from mindtrace.hardware.cli.utils.network import (
+    LocalIPError,
+    NetworkError,
+    NoFreePortError,
+    PortCheckError,
+    PortInUseError,
+    ServiceTimeoutError,
+    check_port_available,
+    get_free_port,
+    get_local_ip,
+    get_local_ip_safe,
+    is_port_available,
+    wait_for_service,
+)
 
-__all__ = ["format_status", "print_table", "check_port_available", "get_free_port"]
+__all__ = [
+    "format_status",
+    "print_table",
+    # Network utilities (re-exported from mindtrace.core)
+    "LocalIPError",
+    "NetworkError",
+    "NoFreePortError",
+    "PortCheckError",
+    "PortInUseError",
+    "ServiceTimeoutError",
+    "check_port_available",
+    "get_free_port",
+    "get_local_ip",
+    "get_local_ip_safe",
+    "is_port_available",
+    "wait_for_service",
+]
