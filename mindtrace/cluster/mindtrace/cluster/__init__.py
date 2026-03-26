ad021f9004be4a124c616a70af72293e648d2c1f
Vikrant Sharma <7.sharma.vikrant@gmail.com>
2026-03-26 18:32:13 +0000

    cluster: remove heavy PipelineWorker import from top level init file

--- mindtrace/cluster/mindtrace/cluster/__init__.py
+++ mindtrace/cluster/mindtrace/cluster/__init__.py
@@ -1,5 +1,4 @@
 from mindtrace.cluster.core.cluster import ClusterManager, Node, StandardWorkerLauncher, Worker
-from mindtrace.cluster.core.pipeline_worker import PipelineWorker
 from mindtrace.cluster.core.types import ProxyWorker
 
-__all__ = ["PipelineWorker", "ClusterManager", "Node", "Worker", "ProxyWorker", "StandardWorkerLauncher"]
+__all__ = ["ClusterManager", "Node", "Worker", "ProxyWorker", "StandardWorkerLauncher"]
