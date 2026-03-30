1acb3477d9b42893610466326844621275c5e18b
AgentDosaku <263657692+AgentDosaku@users.noreply.github.com>
2026-03-30 21:08:42 +0100

    core: export Version from mindtrace.core

--- mindtrace/core/mindtrace/core/__init__.py
+++ mindtrace/core/mindtrace/core/__init__.py
@@ -26,6 +26,7 @@ from mindtrace.core.utils.network import (
 )
 from mindtrace.core.utils.system_metrics_collector import SystemMetricsCollector
 from mindtrace.core.utils.timers import Timeout, Timer, TimerCollection
+from mindtrace.registry.core.types import Version
 
 setup_logger()  # Initialize the default logger
 
@@ -65,5 +66,6 @@ __all__ = [
     "TimerCollection",
     "Timeout",
     "SystemMetricsCollector",
+    "Version",
     "wait_for_service",
 ]
